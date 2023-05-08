848ad8140a895ffb680c6c75abd5fbd7371ad57e
zonocan <zonocan.y.m@icloud.com>
2023-05-09 00:35:23 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,101 @@
+// jquery
+$(function() {
+  // ページャー
+  $('.items-fl').paginathing({
+    perPage: 24,
+    prevText: '<i class="fas fa-angle-left"></i>',
+    nextText: '<i class="fas fa-angle-right"></i>',
+    activeClass: 'navi-active',
+  });
+});
+
+// アコーディオン
+
+// slideUp
+const slideUp = (el, duration = 300) => {
+  el.style.height = el.offsetHeight + "px";
+  el.offsetHeight;
+  el.style.transitionProperty = "height, margin, padding";
+  el.style.transitionDuration = duration + "ms";
+  el.style.transitionTimingFunction = "ease";
+  el.style.overflow = "hidden";
+  el.style.height = 0;
+  el.style.paddingTop = 0;
+  el.style.paddingBottom = 0;
+  el.style.marginTop = 0;
+  el.style.marginBottom = 0;
+  setTimeout(() => {
+    el.style.display = "none";
+    el.style.removeProperty("height");
+    el.style.removeProperty("padding-top");
+    el.style.removeProperty("padding-bottom");
+    el.style.removeProperty("margin-top");
+    el.style.removeProperty("margin-bottom");
+    el.style.removeProperty("overflow");
+    el.style.removeProperty("transition-duration");
+    el.style.removeProperty("transition-property");
+    el.style.removeProperty("transition-timing-function");
+  }, duration);
+};
+
+// slideDown
+const slideDown = (el, duration = 300) => {
+  el.style.removeProperty("display");
+  let display = window.getComputedStyle(el).display;
+  if (display === "none") {
+    display = "block";
+  }
+  el.style.display = display;
+  let height = el.offsetHeight;
+  el.style.overflow = "hidden";
+  el.style.height = 0;
+  el.style.paddingTop = 0;
+  el.style.paddingBottom = 0;
+  el.style.marginTop = 0;
+  el.style.marginBottom = 0;
+  el.offsetHeight;
+  el.style.transitionProperty = "height, margin, padding";
+  el.style.transitionDuration = duration + "ms";
+  el.style.transitionTimingFunction = "ease";
+  el.style.height = height + "px";
+  el.style.removeProperty("padding-top");
+  el.style.removeProperty("padding-bottom");
+  el.style.removeProperty("margin-top");
+  el.style.removeProperty("margin-bottom");
+  setTimeout(() => {
+    el.style.removeProperty("height");
+    el.style.removeProperty("overflow");
+    el.style.removeProperty("transition-duration");
+    el.style.removeProperty("transition-property");
+    el.style.removeProperty("transition-timing-function");
+  }, duration);
+};
+
+// slideToggle
+const slideToggle = (el, duration = 300) => {
+  if (window.getComputedStyle(el).display === "none") {
+    return slideDown(el, duration);
+  } else {
+    return slideUp(el, duration);
+  }
+};
+
+//.search-formのDOM取得
+const form = document.querySelector('.search-form');
+
+// .search-area-btnのDOM取得
+const slideToggleBtn = document.querySelector(".search-area-btn");
+
+// UIのDOM取得
+const slideToggleBtnUi = document.querySelector(".search-area-btn-ui");
+
+// アコーディオン実行
+slideToggleBtn.addEventListener("click", () => {
+  slideToggle(form, 300);
+  slideToggleBtnUi.classList.toggle('is-active');
+});
+
+// searchformのリセットボタン
+function searchFormReset() {
+  document.searchform.reset();
+}
